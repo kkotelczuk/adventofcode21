80ac122afd80458291bb9e1a2c039c53e28cb508
kkotelczuk <konrad.kotelczuk@bitbay.net>
2021-12-13 15:54:18 +0100

    day 1 - part 2

--- 01/index.js
+++ 01/index.js
@@ -3,10 +3,11 @@ const fs = require("fs").promises;
 (async () => {
   const data = (await fs.readFile("../data/01/input.txt")).toString();
   const values = data.split("\n").map((v) => +v);
-  solution(values);
+  const result = slidingWindow(values);
+  console.log(result);
 })();
 
-const solution = (input) => {
+const basic = (input) => {
   let prevValue = input[0];
   let count = 0;
   for (let i in input) {
@@ -15,5 +16,18 @@ const solution = (input) => {
     }
     prevValue = input[i];
   }
-  console.log("count", count);
+  return count;
+};
+
+const slidingWindow = (input) => {
+  let windowA = input[0] + input[1] + input[2];
+  let result = 0;
+  for (let i in input) {
+    if (+i + 2 > input.length) return result;
+    let windowB = input[i] + input[+i + 1] + input[+i + 2];
+    if (windowB > windowA) {
+      result++;
+    }
+    windowA = windowB;
+  }
 };
